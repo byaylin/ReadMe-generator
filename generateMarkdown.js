0bc7f4e6d4d676977d4b6dd4ceac5414ab61f91e
byaylin <aylinxmorales@gmail.com>
2023-01-23 14:15:27 -0800

    updates

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,21 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-  // turns into html markdown 
-
-`;
+function generateMarkdown(htmlData){
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      <h1> $(data.title) </h1>
+      <h3> $(data.name) </h3>
+      <h2>Description</h2>
+          <p>$(data.description)</p>
+      <h2>Installation</h2>
+          <p>$(data.installation)</p>
+      <h2>Usage</h2>
+          <p>$(data.usage)</p>
+      <h2>Tests</h2>
+          <p> $(data.test) </p>
+      <h2>License</h2>
+          <p> $(data.license)</p>
+      <h2> Questions</h2>
+          <p>For addition questions please contact me through links below</p>
+          <ul>
+              <li>$(data.github)</li>
+              <li>$(data.email)</li>
+          </ul>
+          
+  </body>
+  </html>`
 }
 
-module.exports = generateMarkdown;
+modules.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -2,161 +2,79 @@
 //to write file 
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('generateMarkdown.js');
+//const generateMarkdown = require('generateMarkdown.js');
 
 // Create an array of questions for user input
-const questions = [
-    {
-        type: 'input', 
-        name: 'title',
-        message: 'Welcome to the ReadMe Generator! Please enter the title of your project',
-        validate: titleInput =>{
-            if (titleInput){
-                return true;
-            } else {
-                console.log('Your project must have a title, please try again')
-                return false;
-            }
+inquirer.prompt([
+        {
+            type: 'input', 
+             name: 'title',
+            message: 'Welcome to the ReadMe Generator! Please enter the title of your project',
+        },
+        {
+            type: 'input', 
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input', 
+            name: 'github',
+            message: 'What is your Gitub username?',
+         },
+         {
+            type: 'input', 
+            name: 'email',
+            message: 'What is your email?',
+         },
+        {
+            type: 'input', 
+            name: 'description',
+            message: 'Enter the description of your project',
+        },
+        {
+            type: 'input', 
+            name: 'installation',
+            message: 'Enter the installation instructions of your project',
+        },
+        {
+            type: 'input', 
+            name: 'usage',
+            message: 'Enter usage information of your project',
+        },
+        {
+            type: 'input', 
+            name: 'contributions',
+            message: 'Enter contribution guidelines of your project',
+        },
+        {
+            type: 'input', 
+            name: 'test',
+            message: 'Enter test instructions of your project',
+        },
+        {
+            type: 'confirm', 
+            name: 'confirmLicense',
+            message: 'Would you like to include a license?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['MIT','ISC','zLib','Academic Free License'],
         }
+])
 
-    },
-    {
-        type: 'input', 
-        name: 'name',
-        message: 'What is your name?',
-        validate: nameInput =>{
-            if (nameInput){
-                return true;
-            } else {
-                console.log('Please enter your name')
-                return false;
+ function init(){
+    inquirer.prompt(questions).then(function(answers){
+        const htmlData = generateMarkdown(answers)
+        fs.writeFile('index.html', htmlData, function (err){
+            if(err){
+                console.log(err)
+            }else{
+                console.log("Ta-da")
             }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'GitHub',
-        message: 'What is your Gitub username?',
-        validate: githubInput =>{
-            if (githubInput){
-                return true;
-            } else {
-                console.log('You must have a GitHub profile, please try again')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'description',
-        message: 'Enter the description of your project',
-        validate: descriptionInput =>{
-            if (descriptionInput){
-                return true;
-            } else {
-                console.log('Your project must have a description, please try again')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'installation',
-        message: 'Enter the installation instructions of your project',
-        validate: installInput =>{
-            if (installInput){
-                return true;
-            } else {
-                console.log('Please provide instructions for installation to run your program')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'usage',
-        message: 'Enter usage information of your project',
-        validate: usageInput =>{
-            if (usageInput){
-                return true;
-            } else {
-                console.log('Please provide usage instructions to help users navigate your project')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'contributions',
-        message: 'Enter contribution guidelines of your project',
-        validate: contributionInput =>{
-            if (contributionInput){
-                return true;
-            } else {
-                console.log('Please provide guidelines for others to contribute to your project')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'input', 
-        name: 'name',
-        message: 'Enter test instructions of your project',
-        validate: contributionInput =>{
-            if (contributionInput){
-                return true;
-            } else {
-                console.log('You must include test instructions, please try again')
-                return false;
-            }
-        }
-
-    },
-    {
-        type: 'confirm', 
-        name: 'confirmLicense',
-        message: 'Would you like to include a license?',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        choices: ['MIT','ISC','zLib','Academic Free License'],
-        when: licenseChosen =>{
-            if (licenseChosen){
-                return true;
-            }else {
-                return false; 
-            }
-        }
-    }
-]
-
-//Create a function to write README file
-fs.appendFile('generateMarkdown.js', questions, err =>{
-    if(err){
-        console.error(err);
-    }
-});
-
-//Create a function to initialize app
-const init = () =>{
-    return inquirer.createPromptModule(questions);
-}
-
-// Function call to initialize app
-init()
-    .then(userInput => {
-        return generateMarkdown(userInput);
-    }) 
-    .then (readmeInfo =>{
-        return fs.writeFile(readmeInfo);
+        })
     })
-    .catch (err =>{
-        console.log(err);
-    })
+ }
+
+ init();
+
